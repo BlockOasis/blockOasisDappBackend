5eecb2de6d17d38b7d2ecb31ee093353b92f104b
Aryamanraj <20cs3011@rgipt.ac.in>
2023-12-03 18:24:11 +0530

    withdraw function comments added

--- routes/withdraw_route.py
+++ routes/withdraw_route.py
@@ -12,16 +12,34 @@ logging.basicConfig(level=logging.DEBUG)
 
 # {'action': 'withdraw', 'amount': '500000000000000'}
 def withdraw(json_router):
+    """
+    Registers a route for handling withdrawal requests in the JSON router.
+
+    Args:
+        json_router: The router object that manages JSON route handling.
+    """
     @json_router.advance({'action': 'withdraw'})
     def withdraw(rollup: Rollup, data: RollupData) -> bool:
-        LOGGER.info("Withdraw request")
+        """
+        Handles a withdrawal request based on the provided Rollup data.
 
-        payload = WithdrawInput.parse_obj(data.json_payload())
+        Args:
+            rollup (Rollup): The Rollup context.
+            data (RollupData): Data related to the withdrawal request.
+
+        Returns:
+            bool: True if the withdrawal handling is successful, False otherwise.
+        """
+        LOGGER.info(f"Withdraw request: {data}")
+
+        # Validate the withdrawal request payload
+        payload = WithdrawInput.model_validate(data.json_payload())
         
+        # Extract user and amount from the payload
         user = data.metadata.msg_sender
         amount = payload.amount
 
-        # remove from wallet
+        # Attempt to withdraw the specified amount from the user's bank account
         try: 
             bank_db.withdraw(user,amount)
         except Exception as e:
@@ -30,11 +48,11 @@ def withdraw(json_router):
             rollup.report(str2hex(msg))
             return False
 
-        # generate voucher
+        # Generate a voucher for the ERC20 transfer
         voucher = create_erc20_transfer_voucher(settings.TOKEN_ERC20_ADDRESS,user,amount)
         rollup.voucher(voucher)
 
-        # send notice with current balance
+        # Send a notice with the current balance after withdrawal
         rollup.notice(str2hex(f"{{\"action\":\"withdraw\",\"address\":\"{user}\",\"balance\":\"{bank_db.balance(user)}\"}}"))
 
         LOGGER.info(f"Withdrawing {amount} for user {user}")
