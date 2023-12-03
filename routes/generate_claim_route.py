02f953e180f576443b01601e3954278321fb1b62
Aryamanraj <20cs3011@rgipt.ac.in>
2023-12-03 18:22:47 +0530

    claim route

--- routes/generate_claim_route.py
+++ routes/generate_claim_route.py
@@ -1,64 +1,64 @@
 import logging
-
 from helpers.utils import to_jsonhex
 from cartesi import Rollup, RollupData
 
 from model.bank_db import bank_db
-
 from model.user_db import users_db
 from model.claims_db import claims_db
-
 from helpers.setting import settings
 from helpers.inputs import ClaimInput
 
-
+# Configure logging for the module
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
 def generate_claim(json_router):
-    # def decode_payload(decoded_data, data: RollupData):
-    #     """
-    #     Decodes the JSON payload from the data and extracts necessary fields.
+    """
+    Registers the claim generation route and its handler in the provided JSON router.
 
-    #     Args:
-    #         data (RollupData): The data object containing the JSON payload.
-
-    #     Returns:
-    #         dict: A dictionary containing extracted fields from the payload.
-    #     """
-    #     prep_CID = decoded_data.get("cidPrep")
-    #     comp_CID = decoded_data.get("cidComp")
-    #     collateral = decoded_data.get("coll")
-    #     comp_proof = decoded_data.get("compProof")
-    #     user_wallet_address = data.metadata.msg_sender
-    #     timestamp = data.metadata.timestamp
-
-    #     """ Example:
-    #     {"handle": "claim","cidPrep": "QmdsL7GxAPRbYh9U7mG6ctnjhbChC7Yws4oriSxUafQ41R","cidComp": "QmdsL7GxAPRbYh9U7mG6ctnjhbChC7Yws4oriSxUafQ41R","coll": "1000000000000000000","compProof": "QmdsL7GxAPRbYh9U7mG6ctnjhbChC7Yws4oriSxUa"}
-    #     """
-
-    #     return {
-    #         "prep_CID": prep_CID,
-    #         "comp_CID": comp_CID,
-    #         "collateral": collateral,
-    #         "comp_proof": comp_proof,
-    #         "user_wallet_address": user_wallet_address,
-    #         "timestamp": timestamp,
-    #     }
+    Args:
+        json_router: The router object that manages JSON route handling.
+    """
 
     def validate_collateral(collateral: int):
+        """
+        Validates whether the provided collateral is correct.
+
+        Args:
+            collateral (int): The amount of collateral to be validated.
+
+        Returns:
+            (bool, str): Tuple of validation status and message.
+        """
         if collateral is None or collateral != settings.COLLATERAL_AMOUNT:
             message = f"Collateral amount should be equal {settings.COLLATERAL_AMOUNT}"
             return False, message
         return True, ""
 
     def check_existing_claim(prep_CID: str):
+        """
+        Checks if a claim with the given preparation CID already exists.
+
+        Args:
+            prep_CID (str): The preparation CID to check for existing claims.
+
+        Returns:
+            (bool, str): Tuple of check status and message.
+        """
         if claims_db.get_claim_by_prep_CID(prep_CID):
             return False, "Claim already exists"
         return True, ""
 
     def check_and_lock_funds(user_wallet_address: str):
+        """
+        Checks if the user has enough funds and locks the required collateral amount.
+
+        Args:
+            user_wallet_address (str): The wallet address of the user.
+
+        Returns:
+            (bool, str): Tuple of fund check status and message.
+        """
         if bank_db.balance(user_wallet_address) < settings.COLLATERAL_AMOUNT:
             message = f"Insufficient funds for collateral in user's wallet. Current Balance: {bank_db.balance(user_wallet_address)}"
             return False, message
@@ -81,11 +81,25 @@ def generate_claim(json_router):
         comp_CID: str,
         value: int,
     ):
+        """
+        Creates a new claim and registers it in the claims database.
+
+        Args:
+            prep_CID (str): Preparation CID of the claim.
+            user_address (str): User's wallet address.
+            timestamp (str): Timestamp of the claim.
+            comp_proof (str): Computation proof of the claim.
+            comp_CID (str): Computation CID of the claim.
+            value (int): Collateral value of the claim.
+
+        Returns:
+            (int, Claim): Tuple of the new claim ID and the claim object.
+        """
         new_claim_id = claims_db.get_next_claim_id()
         new_claim = claims_db.create_claim(
             prep_CID=prep_CID,
             user_address=user_address,
-            timestamp_of_claim=timestamp,  # Ensure this is correctly formatted
+            timestamp_of_claim=timestamp,
             comp_proof=comp_proof,
             comp_CID=comp_CID,
             value=value,
@@ -93,6 +107,13 @@ def generate_claim(json_router):
         return new_claim_id, new_claim
 
     def update_user_open_claims(user_wallet_address: str, new_claim_id: int):
+        """
+        Updates the user's open claims in the database.
+
+        Args:
+            user_wallet_address (str): The wallet address of the user.
+            new_claim_id (int): The ID of the newly created claim.
+        """
         user = users_db.get_user(user_wallet_address)
         if user:
             user.open_claims[str(new_claim_id)] = None
@@ -100,8 +121,19 @@ def generate_claim(json_router):
 
     @json_router.advance({"handle": "claim"})
     def handle_claim(rollup: Rollup, data: RollupData):
+        """
+        Handles the claim generation based on the provided Rollup data.
+        This function processes the claim generation, updating databases, and handling collateral.
+
+        Args:
+            rollup (Rollup): The Rollup object managing blockchain state.
+            data (RollupData): The data associated with the rollup event.
+
+        Returns:
+            bool: True if the claim is successfully generated, False otherwise.
+        """
         LOGGER.info(f"Handling Claim: {data}")
-        payload = ClaimInput.parse_obj(data.json_payload())        
+        payload = ClaimInput.model_validate(data.json_payload())        
 
         # 1. Validate Collateral
         valid, message = validate_collateral(payload.collateral)
@@ -111,7 +143,7 @@ def generate_claim(json_router):
         LOGGER.info("✅ Collateral Validated")
 
         # 2. Check Existing Claim
-        valid, message = check_existing_claim(payload.CID_prep)
+        valid, message = check_existing_claim(payload.prep_CID)
         if not valid:
             rollup.report(to_jsonhex({"error": message}))
             return False
@@ -139,5 +171,6 @@ def generate_claim(json_router):
         update_user_open_claims(data.metadata.msg_sender, new_claim_id)
         LOGGER.info("Updated user database with open claim")
 
+        # Generate a notice of claim generation
         rollup.notice(to_jsonhex({"key": new_claim_id, "claim": new_claim.dict()}))
         return True
